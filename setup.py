4bc8b173e323c0b7192c7d75dc302515422546d5
yusk <tdfagamdb@yahoo.co.jp>
2021-10-03 18:42:22 +0900

    add setup.py & LICENCE

---
+++ setup.py
@@ -0,0 +1,19 @@
+import os
+import setuptools
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+setuptools.setup(name="open-data-linter",
+                 version="0.2.0",
+                 author="yusk,shiita0903,IidaTakuma",
+                 description="Open Data Linter",
+                 long_description=README,
+                 long_description_content_type="text/markdown",
+                 packages=setuptools.find_packages(),
+                 license='MIT License',
+                 classifiers=[
+                     "Programming Language :: Python :: 3",
+                     "Programming Language :: Python :: 3.6",
+                     "License :: OSI Approved :: MIT License",
+                 ])
